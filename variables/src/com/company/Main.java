6b8eccf10bd619ee99d47de750ebf21c3dbeddfa
Büşra Merve AYDIN <busramerveaydn@gmail.com>
2021-10-04 17:17:13 +0300

    04.10.2021

--- variables/src/com/company/Main.java
+++ variables/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Java küçük büyük harf duyarlı bir dildir. (case sensitive)
+        // java camel case ile çalışır yani ilk harf küçük ikinci kelimenin ilk harfi büyük
+
+        System.out.println("Öğrenci sayısı: 9" );
+
+        int studentNumber = 9;
+        System.out.println("Öğrenci sayısı: " + studentNumber);
+
+        String message = "Öğrenci Sayısı: ";
+        System.out.println(message + studentNumber);
     }
 }
